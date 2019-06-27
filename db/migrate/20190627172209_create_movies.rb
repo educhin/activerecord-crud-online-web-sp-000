296f94e2192aabc88ca195a638f012385836e745
Chinedu Arinze <c.s.arinze@gmail.com>
2019-06-27 17:24:17 +0000

    Automatically backed up by Learn

--- db/migrate/20190627172209_create_movies.rb
+++ db/migrate/20190627172209_create_movies.rb
@@ -4,7 +4,8 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string :title
       t.integer :release_date
       t.string :director
-      
+      t.string :lead
+      t.boolean :in_theaters
     end
   end
 end
